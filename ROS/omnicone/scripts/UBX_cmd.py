acd141abf28949f3fccdeabab8f2f92db5520700
Jacob Hartzer <JMHartzer@tamu.edu>
2019-11-18 22:17:32 -0600

    Updating GPS Driving

--- ROS/omnicone/scripts/UBX_cmd.py
+++ ROS/omnicone/scripts/UBX_cmd.py
@@ -12,26 +12,30 @@ from open_base.srv import KinematicsInverse, KinematicsForward
 import serial
 
 
-class UBXCommander:.
+class UBXCommander:
 
     def __init__(self):
 
         rospy.init_node('UBX_cmd')
 
         # Paramters
-        self.circumference = pi * 0.101                     # meters
-        self.linear_to_rot = 2 * pi / self.circumference    # rad / m
-        self.command_timeout = 1.0                          # sec
-        self.rpm = 25.0                                         # rev / minute
+
+        self.lon_home = -96.3550552
+        self.lat_home =  30.6239174
+
+        self.circumference = pi * 0.101                         # meters
+        self.linear_to_rot = 2 * pi / self.circumference        # rad / m
+        self.command_timeout = 1.0                              # sec
+        self.rpm = 30.0                                         # rev / minute
         self.circumference = 3.14159 * 0.101                    # meters
         self.linear_speed = self.rpm / 60 * self.circumference  # m/s
 
         # create the publisher for the goal velocity
         self.vel_pub = rospy.Publisher('robot_vel_goal', Pose2D, queue_size=10)
 
-        self.ubx_gui_cmd      = rospy.Subscriber('/UBX/gui_cmd',  Pose2D,  self.UpdateCmd)
-        self.ubx_hpposllh_sub = rospy.Subscriber('/UBX/hpposllh', Vector3, self.UpdatePos)
-        self.ubx_relpos2D_sub = rospy.Subscriber('/UBX/relpos2D', Pose2D,  self.UpdateHdg)
+        self.ubx_gui_cmd      = rospy.Subscriber('/UBX/gui_cmd',        Pose2D,  self.UpdateCmd)
+        self.ubx_hpposllh_sub = rospy.Subscriber('/UBX/hpposllh/llh',   Vector3, self.UpdatePos)
+        self.ubx_relpos2D_sub = rospy.Subscriber('/UBX/relpos2D/pos',   Pose2D,  self.UpdateHdg)
 
         self.cmd_vel  = Pose2D()
         self.lon = 0
@@ -64,17 +68,22 @@ class UBXCommander:.
                         math.pow( b * math.sin(self.lat) ,2))) + msg.z
 
 
-        dlon = -96.3407028 - self.lon               # degrees
-        dlat =  30.6200939 - self.lat               # degrees
+        dlon = self.lon_home - self.lon             # degrees
+        dlat = self.lat_home - self.lat             # degrees
         dE = R * math.radians(dlon) - self.Xset     # meters
         dN = R * math.radians(dlat) - self.Yset     # meters
-        dT = (180 - self.hdg)
+        dT = 0 # (180 - self.hdg)
+
+        print (dE)
+        print (dN)
+        print (dT)
+        print ("")
 
         distance = ( dE ** 2 + dN ** 2) ** 0.5      # meters
 
-        self.cmd_vel.x = (dE * math.cos(dT) - dN * math.sin(dT)) / max(distance,1) * self.linear_speed
-        self.cmd_vel.y = (dE * math.sin(dT) + dN * math.cos(dT)) / max(distance,1) * self.linear_speed
-        self.cmd_vel.theta  = dT / max(abs(dT),20) * self.linear_speed / 3
+        self.cmd_vel.x = -(dE * math.cos(dT) - dN * math.sin(dT)) / max(distance,1) * self.linear_speed
+        self.cmd_vel.y = -(dE * math.sin(dT) + dN * math.cos(dT)) / max(distance,1) * self.linear_speed
+        # self.cmd_vel.theta  = dT / max(abs(dT),20) * self.linear_speed / 3
 
 
     def run(self):
