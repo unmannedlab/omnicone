Fixed rotation matrices, added condition for end of path

ROS/omnicone/scripts/EKF.py
@@ -162,43 +162,45 @@ class EKF_omnicone:
 
         self.time_curr = rospy.get_time()
 
+        # Determine Rotation Angle of Each Motor 
         # num encoder counts / (4 counts / cycle ) / ( 7 * 27 cycles / rev) (2pi rad / rev)
         theta_l = -float(self.enc_curr[0] - self.enc_prev[0]) / 4 / (7 * 27) * (2 * pi)
         theta_b = -float(self.enc_curr[1] - self.enc_prev[1]) / 4 / (7 * 27) * (2 * pi)
         theta_r = -float(self.enc_curr[2] - self.enc_prev[2]) / 4 / (7 * 27) * (2 * pi)
 
+        # Convert Angular to Linear 
         # v_linear = omega * d / 2
         D1 = theta_l * 0.101 / 2
         D2 = theta_b * 0.101 / 2
         D3 = theta_r * 0.101 / 2
 
-        # Standard transformation from
+        # Forward Kinematics Transformation
         delta_x = (2*D2 - D1 - D3) / 3
         delta_y = math.sqrt(3) * (D3 - D1) / 3
         delta_w = (D1 + D2 + D3)/(3 * 0.1905)
 
         # Rotation to World Frame
-        delta_x_w = delta_x*math.cos(delta_w/2 + self.state[2]) - \
-                    delta_y*math.sin(delta_w/2 + self.state[2])
-        delta_y_w = delta_x*math.sin(delta_w/2 + self.state[2]) + \
-                    delta_y*math.cos(delta_w/2 + self.state[2])
+        delta_x_w = -delta_x*math.cos(delta_w/2 + self.state[2]*pi/180) + \
+                     delta_y*math.sin(delta_w/2 + self.state[2]*pi/180)
+        delta_y_w = -delta_x*math.sin(delta_w/2 + self.state[2]*pi/180) - \
+                     delta_y*math.cos(delta_w/2 + self.state[2]*pi/180)
 
         # Element-wise assign or else assigned by reference (very bad)
         self.enc_prev[0] = self.enc_curr[0]
         self.enc_prev[1] = self.enc_curr[1]
         self.enc_prev[2] = self.enc_curr[2]
 
-        # Position States
+        # Predict Position States
         self.state[0] = delta_x_w + self.state[0]
         self.state[1] = delta_y_w + self.state[1]
         self.state[2] = delta_w   + self.state[2]
 
-        # Velocity States
+        # Predict Velocity States
         self.state[3] = delta_x_w / (self.time_curr - self.time_prev)
         self.state[4] = delta_y_w / (self.time_curr - self.time_prev)
         self.state[5] = delta_w   / (self.time_curr - self.time_prev)
-
-        self.time_curr = self.time_prev
+        
+        self.time_prev = self.time_curr
 
         # P_k|k-1 = F_k * P_k-1|k-1 * F_k^T + Q_k
         self.state_cov = np.matmul(self.Transition_F, np.matmul(self.state_cov, np.transpose(self.Transition_F))) + self.Process_Q
@@ -249,7 +251,10 @@ class EKF_omnicone:
         # State Estimation Publisher
         # Description:
         #     Function transforms state estimate from Kalman filter into Twist
-        #     format and publishes at 10 hz. Is not dependent on callbacks.
+        #     format and publishes at 10 hz. Is not dependent on callbacks. 
+        #     The twist message components are redefined as Pose2D for 
+        #     the linear component and Pose2D's derivative as the 
+        #     angular component. 
 
         rate = rospy.Rate(10) # 10 Hz
         while(not rospy.is_shutdown()):
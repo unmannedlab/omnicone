bb58cdaca764eb2f86c10ab607a1a7d2b6020850
Jacob Hartzer <JMHartzer@tamu.edu>
2019-12-26 12:36:11 -0600

    Fixed rotation matrices, added condition for end of path

--- ROS/omnicone/scripts/EKF.py
+++ ROS/omnicone/scripts/EKF.py
@@ -162,43 +162,45 @@ class EKF_omnicone:
 
         self.time_curr = rospy.get_time()
 
+        # Determine Rotation Angle of Each Motor 
         # num encoder counts / (4 counts / cycle ) / ( 7 * 27 cycles / rev) (2pi rad / rev)
         theta_l = -float(self.enc_curr[0] - self.enc_prev[0]) / 4 / (7 * 27) * (2 * pi)
         theta_b = -float(self.enc_curr[1] - self.enc_prev[1]) / 4 / (7 * 27) * (2 * pi)
         theta_r = -float(self.enc_curr[2] - self.enc_prev[2]) / 4 / (7 * 27) * (2 * pi)
 
+        # Convert Angular to Linear 
         # v_linear = omega * d / 2
         D1 = theta_l * 0.101 / 2
         D2 = theta_b * 0.101 / 2
         D3 = theta_r * 0.101 / 2
 
-        # Standard transformation from
+        # Forward Kinematics Transformation
         delta_x = (2*D2 - D1 - D3) / 3
         delta_y = math.sqrt(3) * (D3 - D1) / 3
         delta_w = (D1 + D2 + D3)/(3 * 0.1905)
 
         # Rotation to World Frame
-        delta_x_w = delta_x*math.cos(delta_w/2 + self.state[2]) - \
-                    delta_y*math.sin(delta_w/2 + self.state[2])
-        delta_y_w = delta_x*math.sin(delta_w/2 + self.state[2]) + \
-                    delta_y*math.cos(delta_w/2 + self.state[2])
+        delta_x_w = -delta_x*math.cos(delta_w/2 + self.state[2]*pi/180) + \
+                     delta_y*math.sin(delta_w/2 + self.state[2]*pi/180)
+        delta_y_w = -delta_x*math.sin(delta_w/2 + self.state[2]*pi/180) - \
+                     delta_y*math.cos(delta_w/2 + self.state[2]*pi/180)
 
         # Element-wise assign or else assigned by reference (very bad)
         self.enc_prev[0] = self.enc_curr[0]
         self.enc_prev[1] = self.enc_curr[1]
         self.enc_prev[2] = self.enc_curr[2]
 
-        # Position States
+        # Predict Position States
         self.state[0] = delta_x_w + self.state[0]
         self.state[1] = delta_y_w + self.state[1]
         self.state[2] = delta_w   + self.state[2]
 
-        # Velocity States
+        # Predict Velocity States
         self.state[3] = delta_x_w / (self.time_curr - self.time_prev)
         self.state[4] = delta_y_w / (self.time_curr - self.time_prev)
         self.state[5] = delta_w   / (self.time_curr - self.time_prev)
-
-        self.time_curr = self.time_prev
+        
+        self.time_prev = self.time_curr
 
         # P_k|k-1 = F_k * P_k-1|k-1 * F_k^T + Q_k
         self.state_cov = np.matmul(self.Transition_F, np.matmul(self.state_cov, np.transpose(self.Transition_F))) + self.Process_Q
@@ -249,7 +251,10 @@ class EKF_omnicone:
         # State Estimation Publisher
         # Description:
         #     Function transforms state estimate from Kalman filter into Twist
-        #     format and publishes at 10 hz. Is not dependent on callbacks.
+        #     format and publishes at 10 hz. Is not dependent on callbacks. 
+        #     The twist message components are redefined as Pose2D for 
+        #     the linear component and Pose2D's derivative as the 
+        #     angular component. 
 
         rate = rospy.Rate(10) # 10 Hz
         while(not rospy.is_shutdown()):
--- ROS/omnicone/scripts/Kinematic_Controller.py
+++ ROS/omnicone/scripts/Kinematic_Controller.py
@@ -14,9 +14,11 @@ from open_base.srv import KinematicsInverse
 class Kinematic_Controller:
 
     def __init__(self):
-        # Title
+        # Kinematic Controller object initialization
         # Description:
-        #     Function does
+        #     Function inializes the Kinematic Controller object. Subscribes 
+        #     to state and path and publsihes commands. Initializes persistent 
+        #     variables.   
 
         rospy.init_node('Kinematic_Controller')
 
@@ -25,95 +27,157 @@ class Kinematic_Controller:
         self.linear_to_rot = 2 * pi / self.circumference    # rad / m
         self.command_timeout = 1.0                          # sec
 
-        # create the publishers
+        # Create the command publishers
         self.left_pub  = rospy.Publisher('left_joint_velocity_controller/command' , Float64, queue_size=10)
         self.back_pub  = rospy.Publisher('back_joint_velocity_controller/command' , Float64, queue_size=10)
         self.right_pub = rospy.Publisher('right_joint_velocity_controller/command', Float64, queue_size=10)
 
-        # create the subscriber
+        # Create the state and path subscribers
         self.state_sub = rospy.Subscriber('/EKF/state', Twist, self.updateVels)
         self.path_sub  = rospy.Subscriber('Waypoints',  Path,  self.updatePath)
 
-
-        # create the caller to the inverse kinematics server
+        # Create the caller to the inverse kinematics server
         self.ik_service_client = rospy.ServiceProxy('/kinematics_inverse_world', KinematicsInverse)
 
-        # initialize persistent variables
+        # Initialize persistent variables
         self.cmd_vel  = [0.0, 0.0, 0.0]
         self.last_cmd_time = rospy.get_time()
         self.Waypoints = Path()
+        self.path_index = -1
 
+        # Kinematic Controller Values
         self.K_err = 0.5
+        self.desired_speed = 0.5
 
 
     def updatePath(self, msg):
-        # Title
+        # Update Desired Path
         # Description:
-        #     Function does
+        #     Function updates the internal desired path for the robot.
 
         self.Waypoints = msg
 
 
     def updateVels(self, msg):
-        # Title
+        # Update Command Velocities
         # Description:
-        #     Function does
+        #     Function uses EKF state information and desired path to publish
+        #     command velocities using a kinematic controller. The nearest point 
+        #     on the path is found and control and error velocities are 
+        #     controlled to that section of the path. 
 
         # Look for closest point and update global velocity goal
-        min = 10^9
-        mindex = 0
-        for i in range(len(self.Waypoints.poses)-1):
-            distance = math.sqrt((self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2 + (self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2)
-            if distance < min:
-                min = distance
-                mindex = i 
-        
+        dist_min = 10^9
+
+        if self.path_index == -1:
+            # If first search of path for closest pose, checks all path poses
+            for i in range(len(self.Waypoints.poses)-1):
+                distance = math.sqrt((self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2 + \
+                                     (self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2)
+                if distance < dist_min:
+                    dist_min = distance
+                    self.path_index = i
+        else:
+            # Otherwise, only checks nearest 20 poses for closest path pose
+            for i in range(max(0,self.path_index-10), \
+                           min(self.path_index+10,len(self.Waypoints.poses)-1)):
+                distance = math.sqrt((self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2 + \
+                                     (self.Waypoints.poses[i].pose.position.x - msg.linear.x)**2)
+                if distance < dist_min:
+                    dist_min = distance
+                    self.path_index = i
+
+        # P0 - Current state position
         x0 = msg.linear.x
         y0 = msg.linear.y
-        x1 = self.Waypoints.poses[mindex  ].pose.position.x
-        x2 = self.Waypoints.poses[mindex+1].pose.position.x
-        y1 = self.Waypoints.poses[mindex  ].pose.position.y
-        y2 = self.Waypoints.poses[mindex+1].pose.position.y
+        # P1 - Closest path pose
+        x1 = self.Waypoints.poses[self.path_index].pose.position.x
+        y1 = self.Waypoints.poses[self.path_index].pose.position.y
+        # P2 - Next path pose
+        x2 = self.Waypoints.poses[self.path_index+1].pose.position.x
+        y2 = self.Waypoints.poses[self.path_index+1].pose.position.y
 
-        a = y1 - y2
-        b = x2 - x1 
-        c = -a*x1 - b*y1 
-        
-        x3 = (b*(b*x0-a*y0) - a*c) / (a**2 + b**2)
-        y3 = (a*(a*y0-b*x0) - b*c) / (a**2 + b**2)
+        if self.path_index == len(self.Waypoints.poses)-2:
+            # If closest to last point, control to final point and stop 
+            dx_world = x2 - x0
+            dy_world = y2 - y0
+            distance = math.sqrt(dx_world**2 + dy_world**2)
+            
+            # Normalizes velocities to error from final point
+            #   Ramps down speed when less than 1 m away
+            Vx = -(dx_world * math.cos(msg.linear.z*pi/180) -   \
+                   dy_world * math.sin(msg.linear.z*pi/180)) /  \
+                       max(distance,1) * self.desired_speed
 
-        dx_path = x2 - x1
-        dy_path = y2 - y1
+            Vy = -(dx_world * math.sin(msg.linear.z*pi/180) +   \
+                   dy_world * math.cos(msg.linear.z*pi/180)) /  \
+                       max(distance,1) * self.desired_speed
 
-        path_norm = math.sqrt( dx_path ** 2 + dy_path ** 2 )
-        Vx_path = dx_path / path_norm
-        Vy_path = dy_path / path_norm
+            # Transform to joint velocities using inverse kinematics
+            goal = Pose2D(Vx,Vy,0)
+            resp = self.ik_service_client(goal)
 
-        Vx_err  = self.K_err * (x3 - x0)
-        Vy_err  = self.K_err * (y3 - y0)        
+            # Convert to rotational speeds
+            self.cmd_vel[0] = -resp.output.v_left  * self.linear_to_rot
+            self.cmd_vel[1] = -resp.output.v_back  * self.linear_to_rot
+            self.cmd_vel[2] = -resp.output.v_right * self.linear_to_rot
 
-        V_world  = math.sqrt((Vx_path + Vx_err)**2 + (Vy_path + Vy_err)**2)
-        Vx_world = (Vx_path + Vx_err) / V_world
-        Vy_world = (Vy_path + Vy_err) / V_world
 
-        Vx = Vx_world * math.cos(msg.linear.z) - Vy_world * math.sin(msg.linear.z)
-        Vy = Vx_world * math.sin(msg.linear.z) + Vy_world * math.cos(msg.linear.z)
+        else:
+            # Calculate Line equation between P1 and P2 in the following form:
+            #     a*x + b*y + c = 0
+            a = y1 - y2
+            b = x2 - x1 
+            c = -a*x1 - b*y1 
+            
+            # P3 - Closest point to P0 on line betweeen P1 and P2
+            x3 = (b*(b*x0-a*y0) - a*c) / (a**2 + b**2)
+            y3 = (a*(a*y0-b*x0) - b*c) / (a**2 + b**2)
 
-        goal = Pose2D(Vx,Vy,0)
-        resp = self.ik_service_client(goal)
+            # Desired path velocity 
+            dx_path = x2 - x1
+            dy_path = y2 - y1
 
-        self.cmd_vel[0] = -resp.output.v_left  * self.linear_to_rot
-        self.cmd_vel[1] = -resp.output.v_back  * self.linear_to_rot
-        self.cmd_vel[2] = -resp.output.v_right * self.linear_to_rot
+            # Normalized path velocity
+            path_norm = math.sqrt( dx_path ** 2 + dy_path ** 2 )
+            Vx_path = dx_path / path_norm
+            Vy_path = dy_path / path_norm
+
+            # Error velocity
+            Vx_err  = self.K_err * (x3 - x0)
+            Vy_err  = self.K_err * (y3 - y0)        
+
+            # Normalize desired and error correction velocities
+            V_world  = math.sqrt((Vx_path + Vx_err)**2 + (Vy_path + Vy_err)**2)
+            Vx_world = (Vx_path + Vx_err) / V_world * self.desired_speed
+            Vy_world = (Vy_path + Vy_err) / V_world * self.desired_speed
+
+            # Transform global control velocity to local control velocity
+            Vx = -Vx_world * math.cos(msg.linear.z*pi/180) + \
+                  Vy_world * math.sin(msg.linear.z*pi/180)
+            Vy = -Vx_world * math.sin(msg.linear.z*pi/180) - \
+                  Vy_world * math.cos(msg.linear.z*pi/180)
+
+            # Transform to joint velocities using inverse kinematics
+            goal = Pose2D(Vx,Vy,0)
+            resp = self.ik_service_client(goal)
+
+            # Convert to rotational speeds
+            self.cmd_vel[0] = -resp.output.v_left  * self.linear_to_rot
+            self.cmd_vel[1] = -resp.output.v_back  * self.linear_to_rot
+            self.cmd_vel[2] = -resp.output.v_right * self.linear_to_rot
 
         self.last_cmd_time = rospy.get_time()
 
     def run(self):
-        # Title
+        # Kinematic Controller Publisher
         # Description:
-        #     Function does
+        #     Function will continually publish control velocities regardless of 
+        #     EKF callback rate based on latest update. Will stop the robot if
+        #     EKF topic is lost for more than 1 second. Publishes commands as 
+        #     individual joint commands.
 
-        rate = rospy.Rate(1) # 1 Hz
+        rate = rospy.Rate(10) # 1 Hz
 
         while(not rospy.is_shutdown()):
             if ( rospy.get_time() - self.last_cmd_time > self.command_timeout):
