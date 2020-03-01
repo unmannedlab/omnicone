fe1cb8edb4ce68149c545edce7758123fab6cc18
Jacob Hartzer <JMHartzer@tamu.edu>
2020-02-29 22:45:51 -0600

    End of Testing Day 1

--- omnicone/scripts/key_teleop.py
+++ omnicone/scripts/key_teleop.py
@@ -60,7 +60,7 @@ class TextWindow():
 
     def write_line(self, lineno, message):
         if lineno < 0 or lineno >= self._num_lines:
-            raise ValueError, 'lineno out of bounds'
+            raise ValueError('lineno out of bounds')
         height, width = self._screen.getmaxyx()
         y = (height / self._num_lines) * lineno
         x = 10
@@ -171,7 +171,7 @@ class KeyTeleop():
         self._interface.write_line(5, 'Use arrow keys to move, space to stop, q to exit.')
         self._interface.refresh()
 
-        twist = self._get_twist(self._linear, self._angular)
+        # twist = self._get_twist(self._linear, self._angular)
         self._pub_cmd.publish(Pose2D)
 
 class SimpleKeyTeleop():
